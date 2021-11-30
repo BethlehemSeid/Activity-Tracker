ebeca77b24515b212e2de6b8e1ca9c74cf7c6ba3
Bethlehem Seid <betty53.msi@gmail.com>
2021-11-30 16:28:40 -0500

    Todo List

--- App.js
+++ App.js
@@ -1,11 +1,144 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function App() {
+
+  const [todos, setTodos] = React.useState([
+  ]);
+  const [textInput, setTextInput] = React.useState('');
+  React.useEffect(() => {
+    getTodosFromUserDevice();
+  }, []);
+  React.useEffect(() => {
+    saveTodoToUserDevice(todos);
+  }, [todos]);
+
+  const addTodo = () => {
+    if (textInput != '') {
+      const newTodo = {
+        id: Math.random(),
+        task: textInput,
+        completed: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTextInput('');
+    }
+  };
+
+  const saveTodoToUserDevice = async todos => {
+    try {
+      const stringifyTodos = JSON.stringify(todos);
+      await AsyncStorage.setItem('todos', stringifyTodos);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getTodosFromUserDevice = async () => {
+    try {
+      const todos = await AsyncStorage.getItem('todos');
+      if (todos != null) {
+        setTodos(JSON.parse(todos));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const markTodoComplete = todoId => {
+    const newTodosItem = todos.map(item => {
+      if (item.id == todoId) {
+        if(item.completed == true){
+          return {...item, completed: false};
+        }else{
+          return {...item, completed: true};
+        }
+      }
+      return item;
+    });
+
+    setTodos(newTodosItem);
+  };
+
+
+  const deleteTodo = todoId => {
+    const newTodosItem = todos.filter(item => item.id != todoId);
+    setTodos(newTodosItem);
+  };
+
+  const clearAllTodos = () => {
+    Alert.alert('Confirm', 'Clear todos?', [
+      {
+        text: 'Yes',
+        onPress: () => setTodos([]),
+      },
+      {
+        text: 'No',
+      },
+    ]);
+  };
+
+  const ListItem = ({todo}) => {
+    return (
+      <View style={styles.listItem}>
+        <View style={{flex: 1}}>
+          <Text
+            style={{
+              fontSize: 15,
+              color: '#000',
+              textDecorationLine: todo?.completed ? 'line-through' : 'none',
+            }}>
+            {todo?.task}
+          </Text>
+        </View>
+
+        
+          <TouchableOpacity onPress={() => markTodoComplete(todo.id)}>
+            <View style={[styles.actionIcon, {backgroundColor: '#A8D4FF'}]}>
+            {!todo?.completed? <Icon name="done" size={20} color="#A8D4FF" /> : <Icon name="done" size={20} color="white" /> }
+            </View>
+          </TouchableOpacity>
+        
+        <TouchableOpacity onPress={() => deleteTodo(todo.id)}>
+          <View style={styles.actionIcon}>
+            <Icon name="delete" size={20} color="#474747" />
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>
+          Activity List
+        </Text>
+        <Icon name="delete" size={20} onPress={clearAllTodos}/>
+      </View>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{padding: 20, paddingBottom: 100}}
+        data={todos}
+        renderItem={({item}) => <ListItem todo={item}/>}
+      />
+      <View style={styles.footer}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            value={textInput}
+            placeholder="Add Todo"
+            onChangeText={text => setTextInput(text)}
+          />
+        </View>
+        <TouchableOpacity onPress={addTodo}>
+          <View style={styles.iconContainer}>
+            <Icon name="add" color="white" size={30} />
+          </View>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +147,65 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff', 
+  },
+  header: {
+    backgroundColor: '#E2E2E2',
+    paddingTop: 60,
+    paddingHorizontal:60,
+    paddingBottom: 20,
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between', 
+  },
+  headerText: {
+    fontSize: 20,
+    color: '#000',
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    color: "#fff",
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  inputContainer: {
+    height: 50,
+    paddingHorizontal: 20,
+    elevation: 40,
+    backgroundColor: '#fff',
+    flex: 1,
+    marginVertical: 20,
+    marginRight: 20,
+    borderRadius: 30,
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#737373',
+    elevation: 40,
+    borderRadius: 25,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  listItem: {
+    padding: 20,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    elevation: 12,
+    borderRadius: 7,
+    marginVertical: 10,
+  },
+  actionIcon: {
+    height: 25,
+    width: 25,
+    // backgroundColor: '#000',
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: 'red',
+    marginLeft: 5,
+    borderRadius: 3,
   },
 });
